232a6af04df4e2e9146b37843cc4bfc6e89f0aad
adebert <debert@gmail.com>
2024-12-06 18:48:35 -0300

    Remove redundant tests for comparison functions in test_comparison.py

--- tests/test_comparison.py
+++ tests/test_comparison.py
@@ -60,34 +60,3 @@ def test_gt(count, other, expected):
     var_count = Rangy(count)
     assert (var_count > other) == expected
 
-@pytest.mark.parametrize("count, other, expected", [
-    (4, 3, True),
-    (4, 4, True),
-    (4, 5, False),
-    ("1-3", 0, True),
-    ("1-3", 1, True),
-    ("1-3", 2, False),
-], ids=[
-    "exact_greater_than_or_equal",
-    "exact_equal",
-    "exact_less_than",
-    "range_greater_than_or_equal",
-    "range_equal",
-    "range_less_than"
-])
-def test_ge(count, other, expected):
-    var_count = Rangy(count)
-    assert (var_count >= other) == expected
-@pytest.mark.skipif(True, "Let's figure out negative values later")
-@pytest.mark.parametrize("count, expected", [
-    ((-1, 3), (-1, 3)),
-    ((3, -1), (-1, 3)),
-    ((-1, -3), (-3, -1)),
-], ids=[
-    "negative_min",
-    "negative_max",
-    "negative_both"
-])
-def test_negative(count, expected):
-    var_count = Rangy(count)
-    assert var_count.values == expected
